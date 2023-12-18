3ad3d10c183aa08f7e51226c476b22d3278ebd03
Simon BREHM <simon.brehm@csilyon.fr>
2023-12-18 15:46:32 +0100

    temp

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, url_for 
+from flask import Flask, render_template, url_for, request, redirect
+from main import *
 
 app = Flask(__name__)
+login_failed = False
 
-@app.route('/')
-
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        input_username = request.form['username']
+        input_password = request.form['password']
+        try:
+            get_data(input_username, input_password)
+            return "success"
+        except:
+            login_failed = True
+            return render_template('login.html', login_failed=login_failed)
+        
+    else:
+        return render_template("login.html")
 
 if __name__=='__main__':
     app.run(debug=True)
