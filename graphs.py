1a7666bc02523ef702adc7aeea225f3fbb81ad3d
Simon Brehm <sim.brehm@gmail.com>
2024-02-06 02:08:53 +0100

    graph implementation

--- graphs.py
+++ graphs.py
@@ -5,13 +5,19 @@ import math
 
 def moyenne_graph(progress):
     progress_percent = progress 
-    start_angle = 0
-    end_angle = progress_percent / 100 * 2 * math.pi  # Convert progress to radians
+    start_angle = math.pi/2
+    end_angle = math.pi/2 + progress_percent / 100 * 2 * math.pi  # Convert progress to radians
 
-    plot = figure(height=300, width=300, title=str(progress), toolbar_location=None, tools="")
+    p = figure(height=350, width=350, title=None, toolbar_location=None, tools="")
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    p.yaxis.visible = False 
+    p.xaxis.visible = False
+    p.outline_line_color = "#FAF9F9"
+    p.background_fill_color = "#FAF9F9"
 
     source = ColumnDataSource(data=dict(start=[start_angle], end=[end_angle]))
-    plot.wedge(x=0, y=0, start_angle='start', end_angle='end', radius=0.8, color="#ff5b5b", source=source)
+    p.wedge(x=0, y=0, start_angle='start', end_angle='end', radius=0.8, color="#ff5b5b", source=source)
 
-    script, div = components(plot)
+    script, div = components(p)
     return script, div
